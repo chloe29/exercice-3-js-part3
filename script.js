da195de516fc288009c452ea09f1088eca9a446d
lavisse <chloe.lavisse80@gmail.com>
2018-10-11 10:03:32 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,15 @@
+//  on déclare la fonction dont l'évènement appelle onclick que l'on a nommé couleur
+// on modifie le document html avec la méthode getElementById en récupérant l'id, on lui applique le style color
+function couleur(){
+  document.getElementById('text').style.color='green';
+}
+//  on déclare la fonction dont l'évènement appelle onclick que l'on a nommé changer
+// on modifie le document html avec la méthode getElementById en récupérant l'id, on lui applique le style color
+function changer(){
+  document.getElementById('text').style.color='red';
+}
+//  on déclare la fonction dont l'évènement appelle onclick que l'on a nommé changement
+// on modifie le document html avec la méthode getElementById en récupérant l'id, on lui applique le style color
+function changement(){
+  document.getElementById('text').style.color='blue';
+}
